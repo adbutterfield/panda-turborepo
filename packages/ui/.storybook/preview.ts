7e03450487ea437bc8b88a0f28ba62a432855f6b
Adam Butterfield <a-butterfield@mercari.com>
2024-06-24 10:17:29 +0900

    add dark mode controls

--- packages/ui/.storybook/preview.ts
+++ packages/ui/.storybook/preview.ts
@@ -2,7 +2,33 @@ import "../src/index.css";
 
 import type { Preview } from "@storybook/react";
 
+const globalTypes = {
+  mode: {
+    name: "Mode",
+    description: "Design System Theme",
+    toolbar: {
+      title: "Mode",
+      icon: "mirror",
+      items: [
+        { value: "light", title: "Light" },
+        { value: "dark", title: "Dark" },
+      ],
+    },
+  },
+};
+
+const withMode = (story, context) => {
+  const { mode } = context.globals;
+  if (mode === "dark" && !document.location.search.includes("--docs")) {
+    document.body.classList.add("dark");
+  } else {
+    document.body.classList.remove("dark");
+  }
+  return story();
+};
+
 const preview: Preview = {
+  globalTypes,
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
@@ -11,7 +37,11 @@ const preview: Preview = {
         date: /Date$/,
       },
     },
+    backgrounds: {
+      disable: true,
+    },
   },
+  decorators: [withMode],
 };
 
 export default preview;
